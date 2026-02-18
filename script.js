e98d687f299930e8eee300e730789a7441c2b570
LuMa369 <luma36912@gmail.com>
2026-02-18 21:48:22 +0100

    Adding Animation&App html
    -Menu Animation Added (Bad code but I couldent find a difrent solution)
    -Creation of all App html files-
    -Menu Animation only when button press
    -Bug to fix(Buttons are
    pressable even when menu is not Extended

---
+++ script.js
@@ -0,0 +1,30 @@
+let opend = false
+function GoTo(Page) {
+  window.location.href = Page + ".html";
+  opend = false;
+}
+
+function imE() {
+  if (opend == false) {
+    for (let i = 0; i < 7; i++) {
+      IconMenuEx = document.getElementsByClassName("IconMenuEx")[i];
+      IconMenuEx.style.animationPlayState = "running";
+      IconMenuEx.style.animationDirection = "normal";
+    }
+    BigMenu = document.getElementsByClassName("BigMenu")[0];
+    Menu.style.animationPlayState = "running";
+    BigMenu.style.animationPlayState = "running";
+    Menu.style.animationDirection = "normal";
+    BigMenu.style.animationDirection = "normal";
+    opend = true;
+  }else if(opend == true){
+        for (let i = 0; i < 7; i++) {
+      IconMenuEx = document.getElementsByClassName("IconMenuEx")[i];
+      IconMenuEx.style.animationDirection = "reverse";
+    }
+    BigMenu = document.getElementsByClassName("BigMenu")[0];
+    Menu.style.animationDirection = "reverse";
+    BigMenu.style.animationDirection = "reverse";
+    opend = false;
+  }
+}
