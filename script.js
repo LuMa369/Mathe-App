c2dd148248efe2ac0670ad0d3daca0ee4213d729
LuMa369 <luma36912@gmail.com>
2026-02-19 00:48:07 +0100

    Add eingabe- + erste- Ableitung
    -Added the Feature that one  can input the "Grad" of a funktion and the funktion itsself
    -Added "Die erste Ableitung".With the data given in the first step the program will show "Die erste ableitung ( f'(x) )

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Navagation
 let opend = false
 function GoTo(Page) {
   window.location.href = Page + ".html";
@@ -32,5 +33,60 @@ function imE() {
     Menu.style.animationDirection = "reverse";
     BigMenu.style.animationDirection = "reverse";
     opend = false;
+  }
+}
+//Ableitung
+function Gradbestimmt(){
+  var n = Number(grad.value);
+  //grad.value = "";
+  EingabeAbleitung.innerHTML = "";
+  EingabeAbleitung.innerHTML += "&#402;(x)="
+    for (let i = n; i >= 0; i--) {
+    if(i == 0){
+      EingabeAbleitung.innerHTML += "<div class='funktion'><input id='"+i+"' class='eingabeAb' type='number'></div>";
+    }else{
+      if(i==1){
+        EingabeAbleitung.innerHTML += "<div class='funktion'><input id='"+i+"' class='eingabeAb' type='number'>x+</div>";
+      }
+      else if(i<=3){
+        EingabeAbleitung.innerHTML += "<div class='funktion'><input id='"+i+"' class='eingabeAb' type='number'>x&sup"+ i +";+</div>";
+      }else if(i<=5) {
+        num = i + 4;
+        EingabeAbleitung.innerHTML += "<div class='funktion'><input id='"+i+"' class='eingabeAb' type='number'>x&#830"+ num +";+</div>";
+      }else if(i<=9) {
+        num = i + 4;
+        EingabeAbleitung.innerHTML += "<div class='funktion'><input id='"+i+"' class='eingabeAb' type='number'>x&#83"+ num +";+</div>";
+      }
+    
+    }
+  }
+  EingabeAbleitung.innerHTML += "<button onclick='ABLBestimmen()'>Ok</button>"
+}
+function ABLBestimmen(){
+  n = Number(grad.value);
+  i = n-1;
+  h = i+1;
+  ErsteABL.innerHTML += "&#402;'(x)="
+    for (let i = n-1; i >= 0; i--) {
+      h = i+1;
+      ABL = document.getElementById(h);
+      alert(ABL.value);
+    if(i == 0){
+      ErsteABL.innerHTML += "<div class='funktion'>"+ ABL.value +"</div>";
+    }else{
+      if(i==1){
+        ErsteABL.innerHTML += "<div class='funktion'>"+ h * ABL.value +"x+</div>";
+      }
+      else if(i<=3){
+        ErsteABL.innerHTML += "<div class='funktion'>"+ h * ABL.value +"x&sup"+ i +";+</div>";
+      }else if(i<=5) {
+        num = i + 4;
+        ErsteABL.innerHTML += "<div class='funktion'>"+ h * ABL.value +"x&#830"+ num +";+</div>";
+      }else if(i<=9) {
+        num = i + 4;
+        ErsteABL.innerHTML += "<div class='funktion'>"+ h * ABL.value +"x&#83"+ num +";+</div>";
+      }
+    
+    }
   }
 }
