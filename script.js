f660dac78bc25ca712577df188ba2ec95838a185
LuMa369 <luma36912@gmail.com>
2026-02-19 23:51:18 +0100

    Added quadratic formula function
    - It now is possible to enter the a, b, and c values of a quadratic formula, and the answer will be output

--- script.js
+++ script.js
@@ -123,3 +123,40 @@ function ABLBestimmen(){
   }
   grad.value = "";
 }
+//Mitternachtsformel
+function Xbestimmen(){
+  preview = document.getElementsByClassName("Preview")[0];
+  preview.style.width = "unset"
+  preview.innerHTML = "<math display='block'><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mrow><mo>−</mo><mi>b</mi></mrow><mo>±</mo><msqrt><mrow><msup><mi>b</mi><mn>2</mn></msup><mo>−</mo><mrow><mn>4</mn><mo>⁢</mo><mi>a</mi><mo>⁢</mo><mi>c</mi></mrow></mrow></msqrt></mrow><mrow><mn>2</mn><mo>⁢</mo><mi>a</mi></mrow></mfrac></mrow></math>"
+  preview.style.width = "80%"
+  Mitternachtsformel = "<math display='block'><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mrow><mo>−</mo><mi>bE</mi></mrow><mo>±</mo><msqrt><mrow><msup><mi>bE</mi><mn>2</mn></msup><mo>−</mo><mrow><mn>4</mn><mo>&#8231;</mo><mi>aE</mi><mo>&#8231;</mo><mi>cE</mi></mrow></mrow></msqrt></mrow><mrow><mn>2</mn><mo>&#8231;</mo><mi>aE</mi></mrow></mfrac></mrow></math>";
+  let a = document.getElementById("a").value;
+  let b = document.getElementById("b").value;
+  let c = document.getElementById("c").value;
+  if(a==""){a=0} if(b==""){b=0} if(c==""){c=0}
+  UwUrzel = b**2 - 4*b*c;
+  for(i = 0;i < 3; i++){
+    Mitternachtsformel = Mitternachtsformel.replace("aE",a);
+    Mitternachtsformel = Mitternachtsformel.replace("bE",b);
+    Mitternachtsformel = Mitternachtsformel.replace("cE",c);
+  }
+  preview.innerHTML += Mitternachtsformel;
+  if(a == 0){
+    AwnserABC.style.gap = "15px"
+    Awnser = -c/b;
+    AwnserABC.innerHTML = "x =  "+ Awnser+" <div>(Für diese Rechnung ist Keine Mitternachts-/ABC-Formel notwendig)</div>";
+  }
+  else if(UwUrzel < 0){ //Keine Lösung
+    AwnserABC.innerHTML = "Keine Lösung (Wurzel Negativ)"
+  }else if(UwUrzel == 0){ //Eine Lösung
+    Awnser = -b/(2*a)
+    AwnserABC.innerHTML = "x =  "+ Awnser; 
+  }else if(UwUrzel>0){ //Zwei Lösung
+    AwnserABC.style.gap = "unset"
+    Awnser1 = (-b + Math.sqrt(UwUrzel))/(2*a);
+    Awnser2 = (-b - Math.sqrt(UwUrzel))/(2*a);
+    AwnserABC.innerHTML = "x<sub>1</sub> =  "+ Awnser1+";   x<sub>2</sub> =  "+Awnser2;
+  }
+
+
+}
